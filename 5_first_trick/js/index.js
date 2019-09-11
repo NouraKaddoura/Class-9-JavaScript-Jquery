fa65a8ff483f7750aa034f85b0d669a38799b744
Noura Kaddoura <nourakaddoura@gmail.com>
2019-09-11 16:34:08 -0700

    Class 9

---
+++ 5_first_trick/js/index.js
@@ -0,0 +1,14 @@
+// This is a Variable - more on what those are later
+//https://www.w3schools.com/js/js_booleans.asp
+
+var circle_is_showing = true;
+
+//you can use 'click' or 'on'
+
+$('#magic').on('click', doMagic)
+
+function doMagic(){
+
+
+
+}
